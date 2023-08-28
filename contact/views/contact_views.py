c2395941ba918dfa6607107809d9e3600f3a282c
LuizEduardo-service <luizeduardo.service@outlook.com>
2023-08-28 08:16:03 -0300

    continuação do projeto agenda

---
+++ contact/views/contact_views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from contact.models import Contact
+
+
+# Create your views here.
+def index(request):
+    contatos = Contact.objects.all()
+
+    context = {
+        'contatos': contatos,
+    }
+    return render(request, 'contact/index.html', context)
